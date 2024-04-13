b720b116c6b6f5aa9ba0eac6999f8aac125bc362
Sichang Sun <krkhssc724@gmail.com>
2024-04-13 20:01:38 +0900

    Tree alignment and docker execution test,sqlboiler code generation

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world")
+	db, err := sql.Open("mysql", "root:root1234@tcp(localhost:3306)/pro-Test")
+	if err != nil {
+		fmt.Println("error occurred. err=", err)
+	}
+	defer db.Close()
+}
