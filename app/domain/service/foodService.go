658ae5b2eb62493258fbeff456bccbdef15d8881
Sichang Sun <krkhssc724@gmail.com>
2024-04-29 13:17:23 +0900

    Implement struct FoodInput in package service

--- app/domain/service/foodService.go
+++ app/domain/service/foodService.go
@@ -1,10 +1,30 @@
 package service
 
+import "github.com/volatiletech/null"
+
 type FoodService struct {
 }
 type FoodOutput struct {
 }
 type FoodInput struct {
+	FoodId         int
+	UserId         int
+	RestaurantName string
+	FoodName       null.String
+	ResAddress     null.String
+	FullAddress    null.String
+	IsLikeflag     int
+	Typecode       int
+	FAddTime       null.Time
+	FUpdataTime    null.Time
+	FirstTime      null.Time
+	TestedFlag     int
+	FoodImg        null.String
+	IsDel          int
+	FoodDetail     null.String
+	OldTags        []int
+	NewAddTags     []int
+	DelTags        []int
 }
 
 // QuerryFoodList
